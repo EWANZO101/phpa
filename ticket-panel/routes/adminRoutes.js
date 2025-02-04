ac7e515a646ed8e595d84bda063ea46620e8292e
EWANZO101 <107235474+EWANZO101@users.noreply.github.com>
2025-02-04 00:28:30 +0000

    Delete ticket-panel directory

--- ticket-panel/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/adminController');
-const authMiddleware = require('../middleware/auth');
-
-router.get('/dashboard', authMiddleware, adminController.getAdminDashboard);
-router.get('/users', authMiddleware, adminController.getUsers);
-
-module.exports = router;
